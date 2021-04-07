eaaf3b654b27b258b085a40a0c658feba4613b97
MiguelED <migueled2695@gmail.com>
2021-04-07 18:30:22 -0500

    get movies en slideshow component

--- src/app/components/slideshow/slideshow.component.ts
+++ src/app/components/slideshow/slideshow.component.ts
@@ -1,17 +1,25 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { movie } from 'src/app/interfaces/cartelera.response';
+
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-slideshow',
   templateUrl: './slideshow.component.html',
   styleUrls: ['./slideshow.component.css']
 })
-export class SlideshowComponent implements OnInit {
+export class SlideshowComponent implements OnInit,AfterViewInit {
   @Input() movies : movie[];
   constructor() { }
 
   ngOnInit(): void {
     console.log(this.movies);
+  }
+
+  ngAfterViewInit(){
+    const swiper = new Swiper('.swiper-container', {
+      loop: true
+    });
   }
 
 }
