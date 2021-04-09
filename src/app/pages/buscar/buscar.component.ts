bf9f6e730d03437163be87da898756b2af31f6ce
MiguelED <migueled2695@gmail.com>
2021-04-09 18:01:38 -0500

    Navegacion de la funcionalidad de buscar

--- src/app/pages/buscar/buscar.component.ts
+++ src/app/pages/buscar/buscar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-buscar',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuscarComponent implements OnInit {
 
-  constructor() { }
+  constructor( private activatedRoute : ActivatedRoute ) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(
+      params => console.log(params.texto)
+      //llamar servicio
+    );
   }
 
 }
